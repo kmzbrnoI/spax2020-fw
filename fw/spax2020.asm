594eb74a554ce901dfe549ab9758aabf122bec3b
Jan Horacek <me@apophis.cz>
2021-05-12 12:04:13 +0200

    Align comments.

--- fw/spax2020.asm
+++ fw/spax2020.asm
@@ -100,23 +100,23 @@ endc
 ; **         START          **
 ; ****************************
 
-            org 0               ; entry point
-            movlw   GP_INI      ; reset GPIO
+            org 0                   ; entry point
+            movlw   GP_INI          ; reset GPIO
             movwf   GPIO
             movwf   GP_ram
-            goto    init        ; goto init
+            goto    init            ; goto init
 
             org 4
-irq:        movwf   tmpw        ; irq
-            swapf   STATUS, w   ; save state
+irq:        movwf   tmpw            ; irq
+            swapf   STATUS, w       ; save state
             movwf   tmps
 
             btfsc   INTCON,GPIF
             goto    irq_dcc
-            nop                 ; unknown interrupt
+            nop                     ; unknown interrupt
             goto    irq_end
 
-irq_dcc:    IRQ_IOC_CLR         ; test DCC pins and set flags
+irq_dcc:    IRQ_IOC_CLR             ; test DCC pins and set flags
             btfsc   DCC_in1
             bsf     fDCC1_hi
             btfss   DCC_in1
@@ -126,11 +126,11 @@ irq_dcc:    IRQ_IOC_CLR         ; test DCC pins and set flags
             btfss   DCC_in2
             bsf     fDCC2_lo
 
-irq_end:    swapf   tmps,W     ; restore state
+irq_end:    swapf   tmps,W          ; restore state
             movwf   STATUS
             swapf   tmpw,f
             swapf   tmpw,w
-            retfie              ; irq end
+            retfie                  ; irq end
 
 irq_notused:
             goto    irq_end
@@ -149,10 +149,10 @@ obn_tab:    ANDLW   0x03
 skok_stav:  MOVF    state,w
             ANDLW   0x03
             ADDWF   PCL, F
-            GOTO    stav_0      ; invalid
-            GOTO    stav_1      ; run
-            GOTO    stav_2      ; poweroff
-            GOTO    stav_3      ; recovery
+            GOTO    stav_0          ; invalid
+            GOTO    stav_1          ; run
+            GOTO    stav_2          ; poweroff
+            GOTO    stav_3          ; recovery
 
 ;state diagram:
 ;    power on = state 0 -> state 1
@@ -166,12 +166,12 @@ skok_stav:  MOVF    state,w
 ; **         INIT           **
 ; ****************************
 
-init:                           ; init
+init:                               ; init
             BANK1
             movlw   GP_TRIS
             movwf   TRISIO
-            call    0x3FF       ; get OSCCAL value; comment out this for simulation
-            movwf   OSCCAL      ; comment out this for simulation
+            ;call    0x3FF          ; get OSCCAL value; comment out this for simulation
+            ;movwf   OSCCAL         ; comment out this for simulation
 ;            nop
 ;            nop
             movlw   IOC_INI
@@ -183,7 +183,7 @@ init:                           ; init
             BANK0
             movlw   CMCON_INI
             movwf   CMCON
-            movlw   0x5f        ; clear RAM (0x20 - 0x5f)
+            movlw   0x5f            ; clear RAM (0x20 - 0x5f)
             movwf   FSR
 clrRAM:     decf    FSR,w
             movwf   FSR
@@ -193,18 +193,18 @@ clrRAM:     decf    FSR,w
             btfss   STATUS, C
             goto    clrRAM
 
-            movlw   T1CON_INI   ; enable timer 1
-            movwf   T1CON       ; |
-            movlw   0xff        ; |
-            movwf   TMR1H       ; |
-            movlw   DCC_post    ; |
-            movwf   DCCtmr_post ; |
+            movlw   T1CON_INI       ; enable timer 1
+            movwf   T1CON           ; |
+            movlw   0xff            ; |
+            movwf   TMR1H           ; |
+            movlw   DCC_post        ; |
+            movwf   DCCtmr_post     ; |
             IRQ_IOC_ENA
 
-            movlw   0xff        ; enable timer 0
-            movwf   TMR0        ; |
+            movlw   0xff            ; enable timer 0
+            movwf   TMR0            ; |
 
-            movlw   INTCON_INI  ; enable interrupts
+            movlw   INTCON_INI      ; enable interrupts
             movwf   INTCON
 init_end:
 
@@ -214,141 +214,152 @@ init_end:
 
 main:       nop
 
-            movfw   GP_ram      ; copy GP_ram to outputs
-            movwf   GPIO        ; |
-            movfw   GPIO        ; read GPIO (for IOC work)
+            movfw   GP_ram          ; copy GP_ram to outputs
+            movwf   GPIO            ; |
+            movfw   GPIO            ; read GPIO (for IOC work)
 
             call    DCCtstfast
             call    handleLed
 
-            CALL    overload_detect
-            BTFSC   foverC
-            GOTO    overload
-            GOTO    ma_0
-
-overload:   BTFSC   foverC
-            GOTO    ma_0          ; do not reset timer when foverC already set
-            BSF     foverC        ; remember shortcut
-            CLRF    TMR1L         ; | reset T1 to init value
-            MOVLW   CTIMET1       ; |
-            MOVWF   TMR1H         ; |
+;            CALL    overload_detect
+;            BTFSC   foverR
+;            GOTO    overload_
+;            GOTO    ma_0
+;
+;overload_:   BTFSC   foverR
+;            GOTO    ma_0           ; do not reset timer when foverR already set
+;            BSF     foverR         ; remember shortcut
+;            CLRF    TMR1L          ; | reset T1 to init value
+;            MOVLW   CTIMET1        ; |
+;            MOVWF   TMR1H          ; |
 
 ma_0:
             ; TIMER 0
-            BTFSS   INTCON,T0IF   ; T0 overflow?
-            GOTO    ma_1          ; no, go to ma_1
+            BTFSS   INTCON,T0IF     ; T0 overflow?
+            GOTO    ma_1            ; no, go to ma_1
 
-T0:         BCF     INTCON,T0IF   ; T0 overflow, clear overflow flag
-            MOVLW   CTIMET0       ; initialize time T0
-            ADDWF   TMR0, F       ; |
+T0:         BCF     INTCON,T0IF     ; T0 overflow, clear overflow flag
+            MOVLW   CTIMET0         ; initialize time T0
+            ADDWF   TMR0, F         ; |
+
+            CALL    overload_detect ; is overload?
+            BTFSC   foverR          ; |
+            GOTO    overload        ; yes,
+            INCF    over_cnt, F     ; no: if (over_cnt > 0) over_cnt--;
+            DECFSZ  over_cnt, F     
+            DECF    over_cnt, F
+
+
+overload:
+
 
 ma_1:
             ; TIMER 1
-            BTFSS   PIR1,TMR1IF   ; T1 overflow?
-            GOTO    main          ; no, loop
+            BTFSS   PIR1,TMR1IF     ; T1 overflow?
+            GOTO    main            ; no, loop
 ; ** T1 **
-T1:         BCF     PIR1,TMR1IF   ; yes, clear overflow flag
-            MOVLW   CTIMET1       ; initialize time T1
-            MOVWF   TMR1H         ; T1 = 4.9152 ms
-            MOVLW   CTIMET1L      ; |
-            ADDWF   TMR1L, F      ; |
+T1:         BCF     PIR1,TMR1IF     ; yes, clear overflow flag
+            MOVLW   CTIMET1         ; initialize time T1
+            MOVWF   TMR1H           ; T1 = 4.9152 ms
+            MOVLW   CTIMET1L        ; |
+            ADDWF   TMR1L, F        ; |
 
-            decfsz  DCCtmr_post   ; postscaler for DCC testing
+            decfsz  DCCtmr_post     ; postscaler for DCC testing
             goto    nodcctst
             movlw   DCC_post
             movwf   DCCtmr_post
-            call    DCCtst        ; solve DCC detection
+            call    DCCtst          ; solve DCC detection
 
 nodcctst:
-            GOTO    skok_stav     ; state machine - begin
+            GOTO    skok_stav       ; state machine - begin
 
             ; invalid state
-stav_0:     MOVLW   1             ; goto state 1
+stav_0:     MOVLW   1               ; goto state 1
             MOVWF   state
             movlw   1
             movwf   short_cnt
             GOTO    T1_end
 
             ; running, all ok
-stav_1:     BTFSC   foverC        ; if (overcurrent)
-            GOTO    calc_OC       ; yes, solve what to do
-            MOVF    short_cnt, W  ; no, test if short_cnt is in normal state (short_cnt == short_t1)
-            SUBLW   short_t1      ; |
-            BTFSS   STATUS,Z      ; |
-            INCF    short_cnt, F  ; no, increment by 1
+stav_1:     BTFSC   foverR          ; if (overcurrent)
+            GOTO    calc_OC         ; yes, solve what to do
+            MOVF    short_cnt, W    ; no, test if short_cnt is in normal state (short_cnt == short_t1)
+            SUBLW   short_t1        ; |
+            BTFSS   STATUS,Z        ; |
+            INCF    short_cnt, F    ; no, increment by 1
             GOTO    T1_end
 calc_OC:
-            DECFSZ  short_cnt, f  ; measure overcurrent time, is it enought?
-            GOTO    T1_end        ; no, do nothing now
-            MOVLW   0x01          ; yes, move to next state (poweroff)
+            DECFSZ  short_cnt, f    ; measure overcurrent time, is it enought?
+            GOTO    T1_end          ; no, do nothing now
+            MOVLW   0x01            ; yes, move to next state (poweroff)
             ADDWF   state,f
-            MOVLW   0x00          ; set first recover interval
-            MOVWF   restore_state ; |
-            CALL    obn_tab       ; |
-            MOVWF   restore_cnt   ; |
+            MOVLW   0x00            ; set first recover interval
+            MOVWF   restore_state   ; |
+            CALL    obn_tab         ; |
+            MOVWF   restore_cnt     ; |
             GOTO    T1_end
 
             ; short cirtuit, track off
-stav_2:     DECFSZ  restore_cnt,f ; measure recovery wait time
-            GOTO    T1_end        ; if (restore_cnt > 0) then wait
+stav_2:     DECFSZ  restore_cnt,f   ; measure recovery wait time
+            GOTO    T1_end          ; if (restore_cnt > 0) then wait
 
             MOVF    restore_state,w ; |
-            SUBLW   d'02'         ; if (restore_state < 3)
-            MOVLW   short_t1      ; |
-            BTFSC   STATUS,C      ; yes, set t1
-            MOVLW   short_t2      ; no, set t2
-            MOVWF   short_cnt     ; |
-            MOVLW   0x01          ; move to next state (recovery)
+            SUBLW   d'02'           ; if (restore_state < 3)
+            MOVLW   short_t1        ; |
+            BTFSC   STATUS,C        ; yes, set t1
+            MOVLW   short_t2        ; no, set t2
+            MOVWF   short_cnt       ; |
+            MOVLW   0x01            ; move to next state (recovery)
             ADDWF   state,f
             GOTO    T1_end
 
             ; short circuit, track on, recovery
-stav_3:     BTFSC   foverC        ; if (overcurrent)
-            GOTO    calc_OC2      ; yes, solve what to do
-            DECFSZ  short_cnt, f  ; no, if (short_cnt == 0)
-            GOTO    T1_end        ; no, do nothing
-            MOVLW   0x00          ; yes, go to state 1 (run)
-            MOVWF   restore_state ; |
-            MOVLW   0x01          ; |
-            MOVWF   state         ; |
+stav_3:     BTFSC   foverR          ; if (overcurrent)
+            GOTO    calc_OC2        ; yes, solve what to do
+            DECFSZ  short_cnt, f    ; no, if (short_cnt == 0)
+            GOTO    T1_end          ; no, do nothing
+            MOVLW   0x00            ; yes, go to state 1 (run)
+            MOVWF   restore_state   ; |
+            MOVLW   0x01            ; |
+            MOVWF   state           ; |
             GOTO    T1_end
 
-calc_OC2:   DECFSZ  short_cnt, f  ; enought time in state 3 and overcurrent ?
-            GOTO    T1_end        ; no, do nothing
-            MOVLW   0x02          ; overcurrent detected, go to state 2
-            MOVWF   state         ; |
+calc_OC2:   DECFSZ  short_cnt, f    ; enough time in state 3 and overcurrent ?
+            GOTO    T1_end          ; no, do nothing
+            MOVLW   0x02            ; overcurrent detected, go to state 2
+            MOVWF   state           ; |
             MOVF    restore_state,w ; if (restore_state < 3)
-            SUBLW   d'02'         ; |
-            BTFSC   STATUS,C      ; |
+            SUBLW   d'02'           ; |
+            BTFSC   STATUS,C        ; |
             INCF    restore_state,f ; yes, restore_state++
-            MOVFW   restore_state ; |
-            CALL    obn_tab       ; get restore_cnt
-            MOVWF   restore_cnt   ; |
+            MOVFW   restore_state   ; |
+            CALL    obn_tab         ; get restore_cnt
+            MOVWF   restore_cnt     ; |
 
             GOTO    T1_end
 
 
 
 T1_end:
-            BCF     drv_en        ; drv_en (output) = !(s_drv_en & fDCCok)
-            BTFSS   s_drv_en       ; |
-            GOTO    main          ; |
-            BTFSS   fDCCok        ; |
-            GOTO    main          ; |
-            BSF     drv_en        ; |
+            BCF     drv_en          ; drv_en (output) = !(s_drv_en & fDCCok)
+            BTFSS   s_drv_en        ; |
+            GOTO    main            ; |
+            BTFSS   fDCCok          ; |
+            GOTO    main            ; |
+            BSF     drv_en          ; |
             GOTO    main
 
 ; ****************************
 ; **       Functions        **
 ; ****************************
 
-DCCtstfast: movfw   flags_DCC     ; fDCCtstok = fDCC1_hi AND fDCC1_lo AND fDCC2_hi AND fDCC2_lo
-            xorlw   0x0f          ; all 4 flags must be set, then set fDCCtstok
+DCCtstfast: movfw   flags_DCC       ; fDCCtstok = fDCC1_hi AND fDCC1_lo AND fDCC2_hi AND fDCC2_lo
+            xorlw   0x0f            ; all 4 flags must be set, then set fDCCtstok
             btfss   STATUS, Z
             bcf     fDCCtstok
             btfsc   STATUS, Z
             bsf     fDCCtstok
-            btfsc   fDCCtstok     ; if fDCCtstok then no more testing
+            btfsc   fDCCtstok       ; if fDCCtstok then no more testing
             IRQ_IOC_DIS
             return
 
@@ -357,36 +368,36 @@ DCCtstfast: movfw   flags_DCC     ; fDCCtstok = fDCC1_hi AND fDCC1_lo AND fDCC2_
 DCCtst:
             IRQ_IOC_CLR
             IRQ_IOC_ENA
-            clrf    flags_DCC     ; DCC test begin
+            clrf    flags_DCC       ; DCC test begin
 
-                                  ; delay DCCok flag
+                                    ; delay DCCok flag
             btfss   fDCCtstok
             goto    DCCnok
-            btfsc   fDCCok        ; test DCCok flag
+            btfsc   fDCCok          ; test DCCok flag
             return
-            incf    DCC_cnt, w    ; inc OK counter
+            incf    DCC_cnt, w      ; inc OK counter
             movwf   DCC_cnt
-            sublw   DCC_CHCK      ; if (DCC_cnt > DCC_CHCK) then
+            sublw   DCC_CHCK        ; if (DCC_cnt > DCC_CHCK) then
             btfsc   STATUS, C
             return
-            bsf     fDCCok        ; set DCCok flag
+            bsf     fDCCok          ; set DCCok flag
             return
 DCCnok:
-            bcf     fDCCok        ; reset DCCok flag
-            clrf    DCC_cnt       ; clear DCC_cnt
+            bcf     fDCCok          ; reset DCCok flag
+            clrf    DCC_cnt         ; clear DCC_cnt
             return
 
 ; ****************************
 
 handleLed:
-            btfsc   s_drv_en       ; copy s_drv_en to led green
+            btfsc   s_drv_en        ; copy s_drv_en to led green
             bsf     led_green
             btfss   s_drv_en
             bcf     led_green
 
-            btfss   foverC        ; copy enable output to led red
+            btfss   foverR          ; copy enable output to led red
             bcf     led_red
-            btfsc   foverC
+            btfsc   foverR
             bsf     led_red
             return
 
@@ -401,7 +412,7 @@ overload_detect:
             MOVLW   0x02
             MOVWF   overload_debouncer
 
-            BTFSC   overC         ; driver overload (int. comp.) ?
+            BTFSC   overC           ; driver overload (int. comp.) ?
             DECF    overload_debouncer
             BTFSC   overC
             DECF    overload_debouncer
@@ -418,12 +429,12 @@ overload_detect:
             BTFSC   overC
             DECF    overload_debouncer
 
-            BTFSC   overload_debouncer, 7  ; highest bit = 1 -> shortcut for >=2 measurements
+            BTFSC   overload_debouncer, 7 ; highest bit = 1 -> shortcut for >=2 measurements
             GOTO    is_overload
-            BCF     foverC
+            BCF     foverR
             return
 is_overload:
-            BSF     foverC
+            BSF     foverR
             return
 
 
